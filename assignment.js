0f5d185d4334857c32bb40472bc258ab1834b30b
Hercules Chrysanthos <hercsma@hotmail.com>
2023-12-17 17:30:52 +0200

    Add check password

--- assignment.js
+++ assignment.js
@@ -1,3 +1,5 @@
+let matchingPassword = false;
+
 function showPassword(passtype) {
   const x = document.getElementById(passtype);
 
@@ -5,8 +7,21 @@ function showPassword(passtype) {
 }
 
 function submitForm(event) {
-  alert('Η φόρμα υποβλήθηκε επιτυχώς!');
-  event.preventDefault();
+  if (matchingPassword) {
+    alert('Η φόρμα υποβλήθηκε επιτυχώς!');
 
-  document.getElementById('application-form').reset();
+    document.getElementById('application-form').reset();
+  }
+  event.preventDefault();
+}
+
+function checkPasswords() {
+  var password = document.getElementById('pass').value;
+  var repeatPassword = document.getElementById('repass').value;
+
+  if (password !== repeatPassword) {
+    alert('Οι κωδικοί πρόσβασης δεν ταιριάζουν.');
+  } else {
+    matchingPassword = true;
+  }
 }
