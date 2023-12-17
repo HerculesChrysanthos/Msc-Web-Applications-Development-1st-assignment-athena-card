959c6d50cc7209183a2ae493e044cd008c9c76cc
Hercules Chrysanthos <hercsma@hotmail.com>
2023-12-17 16:17:20 +0200

    Remove comments

--- assignment.js
+++ assignment.js
@@ -2,11 +2,6 @@ function showPassword(passtype) {
   const x = document.getElementById(passtype);
 
   x.type = 'password' ? 'text' : 'password';
-  //   if (x.type === 'password') {
-  //     x.type = 'text';
-  //   } else {
-  //     x.type = 'password';
-  //   }
 }
 
 function submitForm(event) {
